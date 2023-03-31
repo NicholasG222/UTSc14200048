91617f50e62ff9d529b315944ef76100aa93283a
NicholasG222 <102024864+NicholasG222@users.noreply.github.com>
2023-03-31 08:27:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,26 +18,31 @@ self.addEventListener('install', function(event) {
   });
   self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.open('first-app')
-        .then(function(cache) {
-          return fetch(event.request)
-            .then(function(res) {
-              cache.put(event.request, res.clone());
-              return res;
-            });
-        }).catch(function(err) {
-            return caches.open('first-app')
-              .then(function(cache) {
-                return fetch(event.request)
-                .then(function(res) {
-                    cache.put(event.request, res.clone());
+      caches.match(event.request)
+        .then(function(response) {
+          if (response) {
+            return response;
+          } else {
+            return fetch(event.request)
+              .then(function(res) {
+                return caches.open('first-app')
+                  .then(function(cache) {
+                    cache.put(event.request.url, res.clone());
                     return res;
                   })
+              })
+              .catch(function(err) {
+                return caches.open('first-app')
+                  .then(function(cache) {
+                    cache.put(event.request.url, res.clone());
+                    return res;
+                  });
               });
-            }));
-
-        }
+          }
+        })
     );
+  });
+  
 
   
 
